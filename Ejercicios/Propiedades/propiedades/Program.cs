33c7a9bd73409e553eb8f33825828c3b00d568b6
Dario Mejia <rmejia_92@outlook.com>
2022-07-12 13:33:42 -0600

    Clases Alumnos

---
+++ Ejercicios/Propiedades/propiedades/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace propiedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno();
+            a.PrimerNombre = "Juan";
+            a.SegundoNombre = "Perez";
+
+            Alumno b = new Alumno();
+            b.Id = 2;
+            b.PrimerNombre = "Maria";
+            b.SegundoNombre = "Martinez";
+
+            Alumno c = new Alumno(3);
+            c.PrimerNombre = "Pedro";
+            c.SegundoNombre = "Perez";
+
+            Alumno d = new Alumno("Juan", "Gimenez");
+
+            Console.WriteLine(a.Id);
+            Console.WriteLine(b.Id);
+            Console.WriteLine(c.Id);
+            Console.WriteLine(d.PrimerNombre + " " + d.SegundoNombre);
+        }
+    }
+}
